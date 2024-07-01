94081b96d9445ed8c97e4a83d222505e1848ea55
AttAditya <attachment.aditya@gmail.com>
2024-07-01 11:31:35 +0530

    Login Page Variance

--- index.js
+++ index.js
@@ -2,16 +2,31 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/html');
 
-    res.write(`
-        <html>
-            <head>
-                <title>Node JS Class</title>
-            </head>
-            <body>
-                <h1>Hello World</h1>
-            </body>
-        </html>
-    `);
+    if (req.url === "/login") {
+        res.write(`
+            <html>
+                <head>
+                    <title>Node JS Class</title>
+                </head>
+                <body>
+                    <h1>Hello Login</h1>
+                    <a href="/">Home Page</a>
+                </body>
+            </html>
+        `);
+    } else {
+        res.write(`
+            <html>
+                <head>
+                    <title>Node JS Class</title>
+                </head>
+                <body>
+                    <h1>Hello World</h1>
+                    <a href="/login">Login Page</a>
+                </body>
+            </html>
+        `);
+    }
 
     res.end();
 });
