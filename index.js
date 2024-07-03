02819cf436a7411dc7c7b877e53b7014059eba71
AttAditya <attachment.aditya@gmail.com>
2024-07-03 09:45:30 +0530

    Using express now

--- index.js
+++ index.js
@@ -1,52 +1,12 @@
-const fs = require('fs');
-const http = require('http');
+const express = require("express");
+const app = express();
+const port = 4000;
 
-const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-
-    switch (req.url) {
-        case "/": {
-            res.write(fs.readFileSync("index.html"));
-            break;
-        }
-        case "/login": {
-            res.write(`
-                <html>
-                    <head>
-                        <title>Node JS Class</title>
-                    </head>
-                    <body>
-                        <h1>Hello Login</h1>
-                        <a href="/">Home Page</a>
-                    </body>
-                </html>
-            `);
-
-            break;
-        }
-        default: {
-            res.write(`
-                <html>
-                    <head>
-                        <title>Node JS Class</title>
-                    </head>
-                    <body>
-                        <h1>404</h1>
-                        <a href="/">Index Page</a>
-                        <a href="/login">Login Page</a>
-                    </body>
-                </html>
-            `);
-        }
-    }
-
-    res.end();
+app.get("/", (req, res) => {
+    res.send("Hello World!");
 });
 
-const PORT = 3000;
-const HOST = 'localhost';
-
-server.listen(PORT, HOST, () => {
-    console.log(`Server is running on http://${HOST}:${PORT}/`);
+app.listen(port, () => {
+    console.log(`Serving on http://localhost:${port}`);
 });
 
